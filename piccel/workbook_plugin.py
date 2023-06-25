515834719b8bcfced7b8eadc019faf1ac159cfc3
Thomas Vincent <thomas.tv.vincent@gmail.com>
2023-06-25 10:37:38 -0400

    Clean up before refactoring

--- piccel/workbook_plugin.py
+++
@@ -1,3 +0,0 @@
-class WorkBookPlugin:
-    def __init__(self, workbook):
-        self.workbook = workbook
--- piccel/workbook_plugin_template.py
+++
@@ -1,9 +0,0 @@
-from piccel.workbook_plugin import WorkBookPlugin
-
-class CustomWorkBookPlugin(WorkBookPlugin):
-
-    def __init__(self, workbook):
-        super(WorkBookPlugin, self).__init__()
-
-    def sheet_order(self):
-        return []
